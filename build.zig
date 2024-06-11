7242967dbb15fafe60c55181f8d6de1ceb607e11
Krzysztof Wolicki <der.teufel.mail@gmail.com>
2024-06-11 23:08:45 +0200

    Update to Zig 0.13

--- build.zig
+++ build.zig
@@ -2,11 +2,11 @@ const std = @import("std");
 
 pub fn build(b: *std.Build) void {
     _ = b.addModule("fraxinus", .{
-        .source_file = .{ .path = "src/lib.zig" },
+        .root_source_file = b.path("src/lib.zig"),
     });
 
     const test_exe = b.addTest(.{
-        .root_source_file = .{ .path = "src/lib.zig" },
+        .root_source_file = b.path("src/lib.zig"),
     });
 
     const test_run = b.addRunArtifact(test_exe);
